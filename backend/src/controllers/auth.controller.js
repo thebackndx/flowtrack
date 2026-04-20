f344637b78fe1cf8f558903495d075065bc5d6a6
The Backnd <thebackndx@gmail.com>
2026-04-20 18:18:43 +0530

    Deploy fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -88,7 +88,8 @@ const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "none"
+            sameSite: "none",
+            maxAge : 7 * 24 * 60 * 60 * 1000
         })
 
         res.status(200).json({
